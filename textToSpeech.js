757a79c7998cc997791d1cd5de392fde45a10fd1
prafull epili <prafullepili@gmail.com>
2021-10-07 18:40:06 +0530

    updated

--- textToSpeech.js
+++ textToSpeech.js
@@ -12,19 +12,26 @@ window.speechSynthesis.onvoiceschanged = () => {
 
 
 // Volume
+// showing initial volume
+var volume = document.querySelector("#volume").value;
+speech.volume = volume;
+document.querySelector("#volume-level").innerHTML = volume;
+
 document.querySelector("#volume").addEventListener("input", () => {
     var volume = document.querySelector("#volume").value;
     speech.volume = volume;
-    console.log(speech.volume);
     document.querySelector("#volume-level").innerHTML = volume;
 })
 
 //speed rate
+const rate = document.querySelector("#rate").value;
+speech.rate = rate;
+document.querySelector("#rate-level").innerHTML = rate;
+
 document.querySelector("#rate").addEventListener("input", () => {
     const rate = document.querySelector("#rate").value;
     speech.rate = rate;
     document.querySelector("#rate-level").innerHTML = rate;
-    console.log(speech.rate);
 })
 
 //pitch
@@ -38,21 +45,32 @@ document.querySelector("#pitch").addEventListener("input", () => {
 // change in voices
 document.querySelector("#voices").addEventListener("change", () => {
     speech.voice = voices[document.querySelector("#voices").value];
-    console.log(voice);
 })
 
-
+// start
 document.querySelector("#start").addEventListener("click", () => {
-    speech.text= document.querySelector("textarea").value;
+    window.speechSynthesis.cancel();
+    const table = document.querySelector('#table').value;
+    if (table != "") {
+        printtable = "";
+        for (let i = 1; i < 11; i++) {
+            printtable += `${table} times ${i} = ${parseInt(table) * i}\n`;
+        }
+        document.querySelector("textarea").value = printtable;
+    }
+    speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 });
 
+// pause
 document.querySelector("#pause").addEventListener("click", () => {
     window.speechSynthesis.pause();
 });
+// resume
 document.querySelector("#resume").addEventListener("click", () => {
     window.speechSynthesis.resume();
 });
+// cancel
 document.querySelector("#cancel").addEventListener("click", () => {
     window.speechSynthesis.cancel();
 });
